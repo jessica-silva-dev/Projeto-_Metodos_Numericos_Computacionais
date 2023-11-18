d29b1a4a000ec9b933e83833820b826f37d52745
Jessica Silva <jessica.akyla@gmail.com>
2023-11-18 16:27:00 -0300

    adicionando cores

--- regressao_linear.py
+++ regressao_linear.py
@@ -1,97 +1,134 @@
 # REGRESSÃO LINEAR
 
+from time import sleep
+from colorama import Fore, Style, init
+init()
+
+# DEFININDO AS VARIÀVEIS DAS CORES
+
+verde = Fore.LIGHTGREEN_EX
+azul = Fore.BLUE
+branco = Fore.LIGHTWHITE_EX
+amarelo = Fore.LIGHTYELLOW_EX
+verde_agua = Fore.LIGHTCYAN_EX
+laranja = Fore.LIGHTRED_EX
+reset = Style.RESET_ALL
+
 # DESCRIÇÃO PARA O USUÁRIO
 
 print(" ")
-print("Projeto em Sistemas Inteligentes")
+print(f"{branco}Projetos em Sistemas Inteligentes{reset}")
+sleep(2)
 print(" ")
-print("Direção: Prof° Sergio João Guimaraes Da Silva")
+print(f"{branco}Direção:Prof° Sergio João Guimaraes Da Silva{reset}")
+sleep(2)
 print(" ")
-print("Regressão Linear")
+print(f"{branco}Método: Regressão Linear{reset}")
+sleep(2)
 print(" ")
 
-# DADOS DO USUÁRIO
+# Criando as lista
+x = []
+fx = []
+m = int(input(f"{verde_agua}Informe a quantidade de linhas: {reset}"))
 
-a = float(input('Digite o valor de X: '))
-b = float(input('Digite o valor de X: '))
-c = float(input('Digite o valor de X: '))
-d = float(input('Digite o valor de X: '))
-e = float(input('Digite o valor de X: '))
-f = float(input('Digite o valor de X: '))
-g = float(input('Digite o valor de X: '))
-h = float(input('Digite o valor de X: '))
-i = float(input('Digite o valor de X: '))
-j = float(input('Digite o valor de X: '))
-k = float(input('Digite o valor de f(x): '))
-l = float(input('Digite o valor de f(x): '))
-m = float(input('Digite o valor de f(x): '))
-n = float(input('Digite o valor de f(x): '))
-o = float(input('Digite o valor de f(x): '))
-p = float(input('Digite o valor de f(x): '))
-q = float(input('Digite o valor de f(x): '))
-r = float(input('Digite o valor de f(x): '))
-s = float(input('Digite o valor de f(x): '))
-t = float(input('Digite o valor de f(x): '))
-quantidade = float(input("Digite a quantidade de linhas: "))
+# Criando um loop para pegar os dados do usuário.
+# Condição de parada do loop, se o usuário já colocou os valores, é somente digitar 00 no x e f(x) que vai parar de pedir valores.
 
-# MULTIPLICANDO OS VALORES DE X e F(x) PARA OBTER O VALOR DE x*f(x)
+for i in range(m):
+    a = float(input(f"{verde_agua}Informe o valor de X: {reset}"))
+    b = float(input(f"{verde_agua}Informe o valor de f(x): {reset}"))
+    x.append(a)
+    fx.append(b)
 
-# Valores de x = a até j
-# Valores de f(x) = k até t
+# Multiplicando os valores de X e F(x) para obter a coluna de x*f(x)
+# Criando a lista que vai receber os resultados da multiplicação
 
-ak = a * k
-bl = b * l
-cm = c * m
-dn = d * n
-eo = e * o
-fp = f * p
-gq = g * q
-hr = h * r
-iss = i * s
-jt = j * t
+coluna_xfx = []
 
-# CALCULANDO OS VALORES DE X ELEVADO AO QUADRADO PARA OBTER O X^2
+# Fazendo a multiplicção das colunas
+# Função zip é para fazer essa interação entre listas diferentes
+# Ou seja ela pegou os resultados multiplicados e formou uma nova lista(coluna)
 
-a2 = a ** 2
-b2 = b ** 2
-c2 = c ** 2
-d2 = d ** 2
-e2 = e ** 2
-f2 = f ** 2
-g2 = g ** 2
-h2 = h ** 2
-i2 = i ** 2
-j2 = j ** 2
+for mult_x, mult_fx in zip(x, fx):
+    multiplicar = mult_x * mult_fx
+    coluna_xfx.append(multiplicar)
+       
+# Calculando os valores de X elevado ao quadrado para obter a coluna de X^2
+# Criando lista que vai receber os resultados de x elevado ao quadrado
+
+coluna_xelevado2 = []
+
+# Aqui não usei a função zip, pois estamos ultilizando somente a lista de X
+
+for elevado in x:
+    elevar = elevado ** 2
+    coluna_xelevado2.append(elevar)
 
 # SOMATORIA DE x, f(x), x*f(x) e x^2
 
-xs = a + b + c + d + e + f + g + h + i + j
-fxs = k + l + m + n + o + p + q + r + s + t
-xfxs =  ak + bl + cm + dn + eo + fp + gq + hr + iss + jt
-x2s = a2 + b2 + c2 + d2 + e2 + f2 + g2 + h2 + i2 + j2
+# Criando as variáveis que vai receber os valores das listas
+# Ela começa com zero, para não interferir nos resultados.
 
+soma_x = 0
+soma_fx = 0
+soma_xfx = 0
+soma_x2 = 0
+
+# Usando novamente a função, pois estamos mexendo com várias listas
+
+for sx, sfx, sxfx, sx2 in zip(x, fx, coluna_xfx, coluna_xelevado2):
+    soma_x += sx
+    soma_fx += sfx
+    soma_xfx += sxfx
+    soma_x2 += sx2
+
+print(soma_x)
+print(soma_fx)
+print(soma_xfx)
+print(soma_x2)
 
 # CALCULANDO O VALOR DE A
 
-valor_A = (xs * xfxs - x2s * fxs) / (xs**2 - quantidade * x2s)
+valor_A = (soma_x * soma_xfx - soma_x2 * soma_fx) / (soma_x**2 - m * soma_x2)
 
 # CALCULAMDO O VALOR DE B
 
-valor_B = (xs * fxs - quantidade * xfxs) / (xs**2 - quantidade * x2s)
+valor_B = (soma_x * soma_fx - m * soma_xfx) / (soma_x**2 - m * soma_x2)
 
 # EXIBINDO O RESULTADO FINAL
 
 print(" ")
-print("Calculando....")
+print(f"{laranja}Calculando ...{reset}")
 print(" ")
-print(f'Valor de A: {valor_A}')
-print(f'Valor de B: {valor_B}')
+sleep(2)
+print(f"{laranja}Adquirindo Resultados ...{reset}")
 print(" ")
-print("Grupo: Jéssica de Jesus Da Silva ")
-print("       Tamires Dionizia De Jesus Benardes ")
-print("       Lavínia De Souza Freitas ")
-print("       Marina Romera Amorim ")
-print("       Riani Katelin Da Silva ")
+sleep(2)
+print(f"{laranja}O Resultado da Regressão Linear é: {reset}")
 print(" ")
-input("Para sair aperte 'Enter'")
+sleep(2)
+valor_A_letra = str(valor_A)
+valor_B_letra = str(valor_B)
+print(f"f(x) = ({valor_A:.4f}) - ({valor_B:.4f}x{reset})")
+print(" ")
+sleep(2)
+print(f"{laranja}Participantes do Grupo: {reset}")
+sleep(1)
+print(f"{verde}Tamires Dionizia De Jesus Bernardes --> RA: {reset}")
+sleep(1)
+print(f"{verde}Jéssica De Jesus Da Silva --> RA: 3023200546{reset}")
+sleep(1)
+print(f"{verde}Lavínia De Souza Freitas --> RA: 3023202645{reset}")
+"""
+sleep(1)
+print(f"{verde}Riani Katelin Da Silva --> RA: {reset}")
+"""
+sleep(1)
+print(f"{verde}Marina Romera Amorim --> RA: 3023108400{reset}")
+sleep(1)
+print(f"{verde_agua}Créditos: Ahmad Abouleinin{reset}")
+print(" ")
+sleep(2)
+input(f"{laranja}Para sair aperte 'Enter'!{reset}")
 print(" ")
